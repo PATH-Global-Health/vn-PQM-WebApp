0d7f69ceec426c9590616922a0e1158c0132840c
Long Hoang <longhdt@outlook.com>
2021-01-09 07:49:51 +0700

    feat: indicator model

---
+++ PQM-WebApp/PQM-WebApp.Data/Entities/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PQM_WebApp.Data.Entities
+{
+    public abstract partial class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            IsDeleted = false;
+            DateCreated = DateTime.UtcNow;
+        }
+
+        [Key, Column(Order = 0)]
+        public Guid Id { get; set; }
+        public Boolean IsDeleted { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+
+}
---
+++ PQM-WebApp/PQM-WebApp.Data/Entities/Indicator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PQM_WebApp.Data.Entities
+{
+    public class Indicator : BaseEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public Guid IndicatorGroupId { get; set; }
+        [ForeignKey("IndicatorGroupId")]
+        public virtual IndicatorGroup IndicatorGroup { get; set; }
+    }
+}
