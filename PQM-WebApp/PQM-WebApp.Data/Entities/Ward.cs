a561cc205be374a7e5dd44f5132ebfeb686ed955
Long Hoang <longhdt@outlook.com>
2021-01-09 08:50:18 +0700

    feat: location dimension

---
+++ PQM-WebApp/PQM-WebApp.Data/Entities/Ward.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PQM_WebApp.Data.Entities
+{
+    public class Ward : BaseEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public Guid DistrictId { get; set; }
+        [ForeignKey("DistrictId")]
+        public virtual District District { get; set; }
+    }
+}
