82d5702046d5c8cb9541c197e85a44f6099bd3a7
Long Hoang <longhdt@outlook.com>
2021-01-11 12:03:31 +0700

    feat: add indicator APIs

---
+++ PQM-WebApp/PQM-WebApp/Controllers/IndicatorsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PQM_WebApp.Data.ViewModels;
+using PQM_WebApp.Service;
+
+namespace PQM_WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndicatorsController : ControllerBase
+    {
+        private readonly IIndicatorService _indicatorService;
+
+        public IndicatorsController(IIndicatorService indicatorService)
+        {
+            _indicatorService = indicatorService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rs = _indicatorService.Get();
+            if (rs.Succeed) return Ok(rs.Data);
+            return BadRequest(rs.ErrorMessage);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]IndicatorCreateModel model)
+        {
+            var rs = _indicatorService.Create(model);
+            if (rs.Succeed) return Ok(rs.Data);
+            return BadRequest(rs.ErrorMessage);
+        }
+    }
+}
