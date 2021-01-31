866084d1bc8f0ee147913119ed9066b34605ca87
Long Hoang <longhdt@outlook.com>
2021-01-31 20:31:23 +0700

    feat: login page

---
+++ PQM-WebApp/PQM-WebApp/wwwroot/js/auth.js
@@ -0,0 +1,23 @@
+﻿function logout() {
+    sessionStorage.removeItem('token');
+    window.location.href = '/Login';
+}
+
+function getToken() {
+    return sessionStorage.getItem('token');
+}
+
+function setToken(token) {
+    sessionStorage.setItem('token', token);
+}
+
+function checkAuth() {
+    let token = getToken();
+    if (!token) {
+        logout();
+    }
+}
+
+$(document).ready(() => {
+    checkAuth();
+});
