cc1ff52a029742cd2aec3df8c649f1b54a3f2dd2
Long Hoang <hoangdaithienlong@gmail.com>
2021-03-20 16:46:51 +0700

    feat: init threshold setting page

---
+++ PQM-WebApp/PQM-WebApp/Controllers/SettingController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PQM_WebApp.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
