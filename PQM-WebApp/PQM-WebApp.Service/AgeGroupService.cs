8b1572de887bd79e3afa96fa9cdaa07ed4cd773d
Tran Son Ha <transonha@gmail.com>
2021-03-27 17:48:42 +0700

    implement CRUD for age group

---
+++ PQM-WebApp/PQM-WebApp.Service/AgeGroupService.cs
@@ -0,0 +1,143 @@
+﻿using Mapster;
+using Microsoft.EntityFrameworkCore;
+using PQM_WebApp.Data;
+using PQM_WebApp.Data.Entities;
+using PQM_WebApp.Data.Models;
+using PQM_WebApp.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PQM_WebApp.Service
+{
+    public interface IAgeGroupService
+    {
+        public ResultModel Create(AgeGroupCreateModel model);
+        public ResultModel Get();
+        public ResultModel Update(AgeGroupViewModel model);
+        public ResultModel Delete(AgeGroupViewModel model);
+    }
+
+    public class AgeGroupService : IAgeGroupService
+    {
+        private readonly AppDBContext _dbContext;
+
+        public AgeGroupService(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ResultModel Create(AgeGroupCreateModel model)
+        {
+            var rs = new ResultModel();
+            try
+            {
+                var ageGroup = model.Adapt<AgeGroup>();
+                ageGroup.Id = Guid.NewGuid();
+                _dbContext.AgeGroups.Add(ageGroup);
+                rs.Succeed = _dbContext.SaveChanges() > 0;
+                if (rs.Succeed)
+                {
+                    rs.Data = ageGroup.Adapt<AgeGroupViewModel>();
+                }
+                return rs;
+            }
+            catch (Exception e)
+            {
+                rs.Succeed = false;
+                rs.ErrorMessage = e.InnerException != null ? e.InnerException.Message : e.Message;
+                return rs;
+            }
+        }
+
+        public ResultModel Get()
+        {
+            var rs = new ResultModel();
+            try
+            {
+                rs.Succeed = true;
+                rs.Data = _dbContext.AgeGroups.Select(s => s.Adapt<AgeGroupViewModel>()).ToList();
+                return rs;
+            }
+            catch (Exception e)
+            {
+                rs.Succeed = false;
+                rs.ErrorMessage = e.InnerException != null ? e.InnerException.Message : e.Message;
+                return rs;
+            }
+        }
+
+        public ResultModel Update(AgeGroupViewModel model)
+        {
+            var rs = new ResultModel();
+            try
+            {
+                var ageGroup = _dbContext.AgeGroups.Find(model.Id);
+                if (ageGroup == null)
+                {
+                    rs.Succeed = false;
+                    rs.ErrorMessage = string.Format("Not found age group: {0}", model.Name);
+                }
+                else
+                {
+                    Copy(model, ageGroup);
+                    ageGroup.DateUpdated = DateTime.Now;
+                    _dbContext.AgeGroups.Update(ageGroup);
+                    rs.Succeed = _dbContext.SaveChanges() > 0;
+                    if (rs.Succeed)
+                    {
+                        rs.Data = ageGroup.Adapt<AgeGroupViewModel>();
+                    }
+                }
+                return rs;
+            }
+            catch (Exception e)
+            {
+                rs.Succeed = false;
+                rs.ErrorMessage = e.InnerException != null ? e.InnerException.Message : e.Message;
+                return rs;
+            }
+        }
+
+        public ResultModel Delete(AgeGroupViewModel model)
+        {
+            var rs = new ResultModel();
+            try
+            {
+                var ageGroup = _dbContext.AgeGroups.Find(model.Id);
+                if (ageGroup == null)
+                {
+                    rs.Succeed = false;
+                    rs.ErrorMessage = string.Format("Not found age group: {0}", model.Name);
+                }
+                else
+                {
+                    ageGroup.IsDeleted = true;
+                    ageGroup.DateUpdated = DateTime.Now;
+                    _dbContext.AgeGroups.Update(ageGroup);
+                    rs.Succeed = _dbContext.SaveChanges() > 0;
+                    if (rs.Succeed)
+                    {
+                        rs.Data = ageGroup.Adapt<AgeGroupViewModel>();
+                    }
+                }
+                return rs;
+            }
+            catch (Exception e)
+            {
+                rs.Succeed = false;
+                rs.ErrorMessage = e.InnerException != null ? e.InnerException.Message : e.Message;
+                return rs;
+            }
+        }
+
+        private void Copy(AgeGroupViewModel source, AgeGroup dest)
+        {
+            dest.Name = source.Name;
+            dest.From = source.From;
+            dest.To = source.To;
+            dest.Order = source.Order;
+        }
+    }
+}
