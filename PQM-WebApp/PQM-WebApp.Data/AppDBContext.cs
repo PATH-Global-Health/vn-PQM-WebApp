1a82acbaec330f6be3795254042136802903096a
Long Hoang <longhdt@outlook.com>
2021-04-19 08:22:12 +0700

    feat: change sex -> gender

--- PQM-WebApp/PQM-WebApp.Data/AppDBContext.cs
+++ PQM-WebApp/PQM-WebApp.Data/AppDBContext.cs
@@ -15,19 +15,18 @@ namespace PQM_WebApp.Data
 
         public DbSet<AgeGroup> AgeGroups { get; set; }
         public DbSet<AggregatedValue> AggregatedValues { get; set; }
-        public DbSet<DimDate> DimDates { get; set; }
-        public DbSet<DimMonth> DimMonths { get; set; }
-        public DbSet<DimYear> DimYears { get; set; }
         public DbSet<District> Districts { get; set; }
         public DbSet<Indicator> Indicators { get; set; }
         public DbSet<IndicatorGroup> IndicatorGroups { get; set; }
         public DbSet<KeyPopulation> KeyPopulations { get; set; }
         public DbSet<Province> Provinces { get; set; }
-        public DbSet<Sex> Sex { get; set; }
+        public DbSet<Gender> Gender { get; set; }
         public DbSet<Site> Sites { get; set; }
         public DbSet<ThresholdSetting> ThresholdSettings { get; set; }
         public DbSet<SiteType> SiteTypes { get; set; }
         public DbSet<CategoryAlias> CategoryAliases { get; set; }
+        public DbSet<UndefinedDimValue> UndefinedDimValues { get; set; }
+        public DbSet<UnsolvedDimValue> UnsolvedDimValues { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -35,6 +34,7 @@ namespace PQM_WebApp.Data
             {
                 relationship.DeleteBehavior = DeleteBehavior.Restrict;
             };
+            modelBuilder.Entity<UnsolvedDimValue>().HasKey(sc => new { sc.AggregatedValueId, sc.UndefinedDimValueId });
         }
     }
 
