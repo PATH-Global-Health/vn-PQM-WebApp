48b09631e9ed945e5d02324c4ee6936eea6f3a90
Long Hoang <92716650+bklonghdt@users.noreply.github.com>
2021-11-02 14:32:45 +0700

    hotfix

--- PQM-WebApp/PQM-WebApp/Startup.cs
+++ PQM-WebApp/PQM-WebApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PQM_WebApp.Extensions;
 
 namespace PQM_WebApp
 {
